33b3fe34bddab816276df1315b1703aeb823ec04
hqyaimer <982107030@qq.com>
2019-10-07 18:34:30 +0800

    calendar

---
+++ README.md
@@ -0,0 +1,84 @@
+Calendar 日期控件
+===========================
+
+### 存放于Div的阳历控件，依赖于JQuery、Bootstrap
+
+开始使用
+-------
+
+## 引入依赖
+
+### 引入JQuery
+
+`<script src="http://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>`
+
+### 引入Bootstrap
+
+`<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />`
+
+### 引入Calendar
+
+`<script src="./calendar.js" type="text/javascript" charset="utf8"></script>
+<link rel="stylesheet" href="./calendar.css" />`
+
+## 编写Html
+
+`
+<div class="calendar-container">
+    <div class="calendar-title">
+        <div>
+            <input class="calendar-date" type="date">
+        </div>
+        <div>
+            <div class="calendar-prevbtn"><span class="glyphicon glyphicon-chevron-left"></span></div>
+            <div class="calendar-fdate" data-format="yyyy年MM月"></div>
+            <div class="calendar-nextbtn"><span class="glyphicon glyphicon-chevron-right"></span></div>
+            <div style="clear:both;"></div>
+        </div>
+        <div>
+            <button class="calendar-backbtn btn btn-default">
+                回到今天
+            </button>
+        </div>
+        <div style="float: none;clear: both;"></div>
+    </div>
+</div>
+`
+
+## 编写Javascript
+
+```js
+$('.calendar-container').calendar();
+```
+
+Options 选项
+-------
+
+`title` - 设置标题,默认`['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天']`。
+
+Event 事件
+-------
+
+`choose` - 选中事件,默认为null。
+
+Function 功能
+-------
+
+`rander` - 重绘。
+
+`setDate` - 参数 `tdate` ,支持 `string` 和 `Date` 类型传入,切换到日期月份,但不选中。
+
+`chooseDate` - 参数 `tdate` ,支持 `string` 和 `Date` 类型传入,选中这个日期。
+
+Example 示例
+-------
+
+```js
+$('.calendar-container').calendar({
+	title: ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'],
+	choose:function(item){
+    	console.log(item.attr("data-date"));
+    }
+});
+```
---
+++ calendar.js
@@ -0,0 +1,191 @@
+(function($) {
+    'use serict';
+    var Calendar = function(element, options) {
+        this.$element = $(element);
+        this.element = element;
+        this.init(options);
+    };
+    Calendar.prototype = {
+        $element: null,
+        element: null,
+        choose:null,
+        title: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天'],
+        date: new Date(),
+        ndate: null,
+        init: function(options) {
+            $.extend(true, this, options);
+            this.date = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate(), 0, 0, 0);
+            this.ndate = this.date;
+            this.rander();
+            var element = this.$element;
+            var date = this.date;
+            element.find('.calendar-backbtn').click(function() {
+                var data = element.data('calendar');
+                data.ndate = data.date;
+                data.rander();
+                element.find('.calendar-backbtn').css('display', 'none');
+            });
+            element.find('.calendar-prevbtn').click(function() {
+                var data = element.data('calendar');
+                if(data.ndate.getMonth() == 0){
+                    data.ndate = new Date(data.ndate.getFullYear() - 1, 11, 1, 0, 0, 0);
+                }else{
+                    data.ndate = new Date(data.ndate.getFullYear(), data.ndate.getMonth() - 1, 1, 0, 0, 0);
+                }
+                data.rander();
+            });
+            element.find('.calendar-nextbtn').click(function() {
+                var data = element.data('calendar');
+                if(data.ndate.getMonth() == 11){
+                    data.ndate = new Date(data.ndate.getFullYear() + 1, 0, 1, 0, 0, 0);
+                }else{
+                    data.ndate = new Date(data.ndate.getFullYear(), data.ndate.getMonth() + 1, 1, 0, 0, 0);
+                }
+                data.rander();
+            });
+            element.find('.calendar-date').change(function(){
+                console.log($(this).val());
+                var date = new Date(Date.parse($(this).val()));
+                var data = element.data('calendar');
+                data.ndate = date;
+                data.rander();
+                data.chooseDate(date);
+            });
+        },
+        rander: function() {
+            $('.calendar-date').val(this.ndate.format('yyyy-MM-dd'));
+            var fdate = $('.calendar-fdate').attr("data-format");
+            $('.calendar-fdate').html(this.ndate.format(fdate));
+            var element = this.$element;
+            var table = element.find('.calendar-table');
+            if (table.length > 0) {
+                table.html("");
+            } else {
+                element.append('<div class="calendar-table"></div>');
+                table = element.find('.calendar-table');
+            }
+            //console.log(this.date);
+            var cdate = this.date;
+            var date = this.ndate;
+            var tdate = new Date(date.getFullYear(), date.getMonth(), 1, 0, 0, 0);
+            var nextmonth = date.getMonth() + 1 > 11 ? 0 : date.getMonth() + 1;
+            var html = "<table><thead><tr>";
+            for (var i = 0; i < 7; i++) {
+                html += "<th>" + this.title[i] + "</th>";
+            }
+            html += "</tr></thead>"
+            html += "<body><tr>";
+            var day = tdate.getDay() == 0 ? 6 : tdate.getDay() - 1;
+            for (var i = 0; i < day; i++) {
+                var temp = new Date(tdate.getTime());
+                temp.setDate(tdate.getDate() - (day - i));
+                html += '<td class="prevtd" data-date="' + temp.format('yyyy-MM-dd') + '">' + temp.getDate() + '</td>';
+            }
+            var rowcount = 1;
+            do {
+                //console.log(tdate.format('yyyy-MM-dd'));
+                if (cdate.getTime() == tdate.getTime()) {
+                    html += '<td class="active currtd" data-date="' + tdate.format('yyyy-MM-dd') + '">' + tdate.getDate() + '</td>';
+                } else {
+                    html += '<td class="currtd" data-date="' + tdate.format('yyyy-MM-dd') + '">' + tdate.getDate() + '</td>';
+                }
+                if (tdate.getDay() == 0) {
+                    html += "</tr><tr>";
+                    rowcount++;
+                }
+                tdate.setDate(tdate.getDate() + 1);
+            } while (tdate.getMonth() != nextmonth);
+            var tday = tdate.getDay() == 0 ? 6 : tdate.getDay() - 1;
+            var max = rowcount < 6 ? 14 : 7;
+            for (var i = tday; i < max; i++) {
+                html += '<td class="nexttd" data-date="' + tdate.format('yyyy-MM-dd') + '">' + tdate.getDate() + '</td>';
+                if (tdate.getDay() == 0) {
+                    html += "</tr><tr>";
+                }
+                tdate.setDate(tdate.getDate() + 1);
+            }
+            html += "</tr></body></table>";
+            table.append(html);
+            var parent = this;
+            table.find('td.currtd').click(function(e) {
+                var sdate = $(this).attr('data-date');
+                var tdate = new Date(Date.parse(sdate));
+                parent.chooseDate(tdate);
+            });
+            table.find('td.prevtd,td.nexttd').click(function(e) {
+                var sdate = $(this).attr('data-date');
+                var tdate = new Date(Date.parse(sdate));
+                parent.setDate(tdate);
+                parent.chooseDate(tdate);
+            });
+        },
+        setDate: function(tdate) {
+            var date = this.date;
+            if(typeof tdate == 'string') date = new Date(Date.parse(tdate));
+            if(typeof tdate == 'object') date = tdate;
+            this.ndate = date;
+            this.rander();
+        },
+        chooseDate: function(tdate){
+            var date = this.date;
+            if(typeof tdate == 'string') date = new Date(Date.parse(tdate));
+            if(typeof tdate == 'object') date = tdate;
+            date.setHours(0);
+            var element = this.$element;
+            var cdate = this.date;
+            var table = element.find('.calendar-table');
+            var tds = table.find('td');
+            var item = null;
+            $.each(tds,function(){
+                var sdate = $(this).attr('data-date');
+                if(sdate){
+                    var tdate = new Date(Date.parse(sdate));
+                    tdate.setHours(0);
+                    if(tdate.getTime() == date.getTime()){
+                        item = $(this);
+                        table.find('td').removeClass('active');
+                        $(this).addClass('active');
+                        if (tdate.getTime() == cdate.getTime()) {
+                            element.find('.calendar-backbtn').css('display', 'none');
+                        } else {
+                            element.find('.calendar-backbtn').css('display', 'block');
+                        }
+                        $('.calendar-date').val(tdate.format('yyyy-MM-dd'));
+                        return;
+                    }
+                }
+            });
+            if(this.choose) this.choose(item);
+        }
+    }
+    $.fn.calendar = function(option, e) {
+        return this.each(function() {
+            var $this = $(this),
+                data = $this.data('calendar'),
+                options = (typeof option == 'object') && option;
+            if (!data) $this.data('calendar', data = new Calendar($this, options));
+            if (typeof option == 'string') data[option].call(data, e);
+        });
+    }
+    $.fn.calendar.Constructor = Calendar;
+})(jQuery);
+Date.prototype.format = function(fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //ÔÂ·Ý
+        "d+": this.getDate(), //ÈÕ
+        "h+": this.getHours(), //Ð¡Ê±
+        "m+": this.getMinutes(), //·Ö
+        "s+": this.getSeconds(), //Ãë
+        "q+": Math.floor((this.getMonth() + 3) / 3), //¼¾¶È
+        "S": this.getMilliseconds() //ºÁÃë
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
+}
